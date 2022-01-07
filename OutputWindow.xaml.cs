ae3a61e8b14f23926d269c9eec46f68ba8defd09
Chris.Duguid <Chris.Duguid@DatacomAU.OnMicrosoft.com>
2022-01-08 00:24:07 +1100

    4 rules working. Shows errors properly. Need to work on further rules, auto emailing errors to team and input validation/error handling.

--- OutputWindow.xaml.cs
+++ OutputWindow.xaml.cs
@@ -24,103 +24,201 @@ namespace TimeSheetParser
     {
 
         string issueMarker = "** ";
-        IEnumerable<Entry> readCSV;
+        List<Entry> readCSV = new List<Entry>();
         List<string> staffList = new List<string>();
         double minimumHoursPerWeek = 40;
 
 
+        static string folderLocation = "Environment.GetFolderPath(Environment.SpecialFolder.Desktop)";
+        static string outputCSVFileName = "csv.csv";
+        static string outputErrorsFilename = "errors.csv";
+        string outputPathCSV = Path.Combine(folderLocation, outputCSVFileName);
+        string outputPathErrors = Path.Combine(folderLocation, outputErrorsFilename);
+
+
+        
 
         public OutputWindow()
         {
             InitializeComponent();
 
-            readCSV = ReadInCSV(MainWindow.selectedfile); //Read in CSV and ensure is valid timesheet.
-        
 
-            createStaffList();
-            runRules();
-
+            ReadInCSV(MainWindow.selectedfile); //Read in CSV and ensure is valid timesheet. 
+            CreateStaffList();
+            RunRules();
+            ruleResults();
 
         }
         //read in CSV
-        IEnumerable<Entry> ReadInCSV(string fileName)
+        private void ReadInCSV(string fileName)
         {
             //Ensure is processed as CSV
-            //string[] lines = File.ReadAllLines(Path.ChangeExtension(fileName, ".csv"));
-            string[] lines = File.ReadAllLines(fileName);
-            return lines.Skip(1).Select(line =>
+            StringBuilder csv = new StringBuilder();
+            if (File.Exists(outputPathCSV))
             {
-                //string[] inputLine = line.Split(',');
+                File.Delete(outputPathCSV);
+            }
+
+            string[] lines = File.ReadAllLines(fileName);
+            foreach(string line in lines.Skip(1))
+            {
                 Regex CSVParse = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
                 string[] inputLine = CSVParse.Split(line);
-                
 
-                //Run rules. 
-                if (rule1(inputLine) == "1")
-                {    
-                    return new Entry(inputLine[0], inputLine[1], inputLine[2], inputLine[3], inputLine[4], inputLine[5], inputLine[6], inputLine[7], inputLine[8], "1", "1");
-                }
-                else
-                {
-                    return new Entry(inputLine[0], inputLine[1], inputLine[2], inputLine[3], inputLine[4], inputLine[5], inputLine[6], inputLine[7], inputLine[8]);
-                }
-
-                return new Entry(inputLine[0], inputLine[1], inputLine[2], inputLine[3], inputLine[4], inputLine[5], inputLine[6], inputLine[7], inputLine[8]);
-            });
+                CreateOutputFile(inputLine, outputCSVFileName);
+                Entry ent = new Entry(inputLine[0], inputLine[1], inputLine[2], inputLine[3], inputLine[4], inputLine[5], inputLine[6], inputLine[7], Convert.ToDouble(inputLine[8]));
+                readCSV.Add(ent);
+            }
         }
-        private void runRules()
-        {
-              rule3();
 
-            ruleResults();
+        //create CSV output file. 
+        private void CreateOutputFile(string[] line, string fileName)
+        {
+            string enteredRow = "";
+            foreach (string item in line)
+            {
+                enteredRow = enteredRow + item + ",";
+            }
+            enteredRow = enteredRow + Environment.NewLine;
+            File.AppendAllText(fileName, enteredRow.ToString());
+        }
+
+        private void RunRules()
+        {
+            rule1(); //ticket number present
+            rule2(); //One ticket number per entry. 
+            rule3(); //description present
+            rule4(); //ticket number format. 
+            rule10(); //check for 40+ hours entered in timesheet. 
         }
 
         //rule 1 function - Required fields. 
         //The main types of work are 1) BAU activity and 2) service delivery
         //BAU work requires a ticket number and a comment at the very least. 
-        //Service delivery requires a comment and no ticket number in ticket reference or description (text with prefix of : CAS, IM, 6 digit Devops, INC-, 
-        public string rule1(string[] row)
+        //Service delivery requires a comment and no ticket number in ticket reference or description (text with prefix of : CAS, IM, 6 digit Devops, INC-)
+        //returns a 1 if support has no ticket number. Returns a 2 if Service delivery and has a ticket number.
+        private void rule1()
         {
-            string res = "0";
-            if (row[3] == "Support")
+            foreach (Entry row in readCSV)
             {
-                if (row[4] == "") // ticket number is empty
+                if (row.TaskName == "Support")
                 {
-                    res = "1";
+                    if (row.TicketNumber == "") // ticket number is empty
+                    {
+                        row.IssueComment = "Ticket # Needed. ";
+                        row.IssueID = 1;
+                    }
+                }
+
+                if (row.TaskName == "Service Delivery")
+                {
+                    if (row.TicketNumber != "") // ticket number has content
+                    {
+                        row.IssueComment = "Remove Ticket #. ";
+                        row.IssueID = 2;
+                    }
                 }
             }
+        }
 
-            if (row[3] == "Service Delivery")
+        //rule 2 function - Only one ticket present in the ticket reference
+        private void rule2()
+        {
+            int numberOfTicketNumbers = 0;
+            foreach (Entry row in readCSV)
             {
-                if (row[4] != "") // ticket number has content
+                //Test for multiple ticket Numbers
+                //Break ticket number field into individual words
+                string[] lineToWords = Regex.Split(row.TicketNumber, @"\s+");
+                //create error
+                if (CountTicketsInEntry(lineToWords) > 1)
                 {
-                    res = "1";
+                    row.IssueID = 2;
+                    row.IssueComment = row.IssueComment + "Only 1 Ticket # per entry. ";
+                }
+            }           
+        }
+
+        //Counts how many ticket numbers there are in an entry and returns an int value.
+        //input is the entry data as string array (string[]) for one cell to check. 
+        private int CountTicketsInEntry(string[] ticketNumberEntry)
+        {
+            int res = 0;
+            foreach (string word in ticketNumberEntry)
+            {
+                if (word != "")
+                {
+                    res++;
                 }
             }
             return res;
         }
 
-        //rule 2 function - Only one ticket present in the ticket reference
-        private bool[] rule2(string[] line)
+        //rule 3 function - make sure there is a description
+        private void rule3()
         {
-            bool[] result = new bool[line.Length + 1];
-            //quick loop to ensure array is full of falses. 
-            for (int i = 0; i < result.Length; i++)
-                result[i] = false;
+            foreach (Entry row in readCSV)
+            {
+               if(row.Description == "")
+                {
+                    row.IssueID = 3;
+                    row.IssueComment = row.IssueComment + "Description needed. ";
+                }
+            }
+        }
+        //rule 4 function - make sure ticket numbers are valid
+        private void rule4()
+        {
+            string casTicketPattern = @"^([Cc][Aa][Ss]-)\d{5}(-).{6}$";
+            string invTicketPattern = @"^([Ii][Nn][Cc]-)\d{5}$"; //INC-ddddd
+            string devOpsTicketPattern = @"^\d{6}$";
+            foreach (Entry row in readCSV)
+            {
+                
+                //Break ticket number field into individual 'words' as string[]
+                string[] lineToWords = Regex.Split(row.TicketNumber, @"\s+");
+                if (CountTicketsInEntry(lineToWords) > 0)
+                {
+                    bool badFormat = true;
+                    foreach(string word in lineToWords)
+                    {
+                        //test patterns against each word
+                        string str = word.Trim('"');
+                        
+                        if (Regex.IsMatch(str, devOpsTicketPattern))
+                        {
+                            badFormat = false;
+                        }
+                        if (Regex.IsMatch(str, casTicketPattern))
+                        {
+                            badFormat = false;
+                        }
+                        if (Regex.IsMatch(word, invTicketPattern))
+                        {
+                            badFormat = false;
+                        }
+                    }
+                    if (badFormat)
+                    {
+                        row.IssueID = 4;
+                        row.IssueComment = row.IssueComment + "Bad ticket/s format. ";
+                    }
+                    badFormat = true;
+                }
+            }
 
 
-
-            return result;
+            //Regex testForTicketNumbers = @  
         }
 
-        //rule 3 function - minimum 40 hours for the week (indicates not filled out timesheet)
+        //rule 10 function - minimum 40 hours for the week (indicates not filled out timesheet)
         //need to pull in the whole lines array and check by staffname and count the hours against activities. 
         //count staff by checking each line and adding twithin an array. 
-        private void rule3()
+        private void rule10()
         {
             bool result = false;
             string staffName;
-            string temp = "";
+            string rawTime = "";
             int counter = 1;
 
             //create a dictionary to hold user and hours
@@ -132,11 +230,12 @@ namespace TimeSheetParser
                 hoursDict.Add(name, 0); //create entry into dictionary
                 foreach (Entry row in readCSV.OfType<Entry>().Where(sn => sn.StaffName == name))
                 {
+                    //rawTime = row.Time.ToString();
+                    //rawTime = rawTime.Replace('"', ' ').Trim();
+                    //rawTime = rawTime.Replace('/', ' ').Trim();
+                    //hoursDict[name] = hoursDict[name] + Convert.ToDouble(rawTime);
+                    hoursDict[name] = hoursDict[name] + row.Time;
 
-                    temp = row.Time;
-                    temp = temp.Replace('"', ' ').Trim();
-                    temp = temp.Replace('/', ' ').Trim();
-                    hoursDict[name] = hoursDict[name] + Convert.ToDouble(temp);
                 }
             }
 
@@ -154,7 +253,6 @@ namespace TimeSheetParser
                 if (!issuefound)
                     txtblkUnfinishedTimesheets.Text = "No Issues Found";
             }
-
         }
 
         private void ruleResults()
@@ -162,8 +260,10 @@ namespace TimeSheetParser
             int counter = 1;
             int numOfIssues = 0;
             foreach (string name in staffList)
+
+            //updates the list of staff with timesheet issues
             {
-                foreach (Entry row in readCSV.OfType<Entry>().Where(iss => iss.Issue == "1").Where(n => n.StaffName == name))
+                foreach (Entry row in readCSV.OfType<Entry>().Where(iss => iss.IssueID != 0).Where(n => n.StaffName == name))
                 {
                     numOfIssues++;
                     ListViewEntries.Items.Add(row);
@@ -177,21 +277,8 @@ namespace TimeSheetParser
                 }
                 
             }
-
-            //output highlighted rows. 
-            //foreach (Entry row in readCSV.OfType<Entry>().Where(iss => iss.Issue == "1"))
-            //{
-            //    if (row.IssueId == "1")
-            //    {
-                   
-            //    }
-                    
-            //   // if (row.IssueId == 2)
-            //   // if (row.IssueId == 3)
-            //   ListViewEntries.Items.Add(row);
-            //}
-
         }
+       
         private string determineTicketNumberInText(string text)
         {
             string result = "";
@@ -206,7 +293,7 @@ namespace TimeSheetParser
             return numStaff;
         }
 
-        private void createStaffList()
+        private void CreateStaffList()
         {
             string staffName;
             foreach (Entry row in readCSV.OfType<Entry>())
@@ -231,12 +318,12 @@ namespace TimeSheetParser
             public string Label { get; set; }
             public string StaffName { get; set; }
             public string Description { get; set; }
-            public string Time { get; set; }
-            public string Issue { get; set; }
-            public string IssueId { get; set; }
+            public double Time { get; set; }
+            public int IssueID { get; set; }
+            public string IssueComment { get; set; }
 
 
-            public Entry(string jobName, string jobNumber, string entryDate, string taskName, string ticketNumber, string label, string staffName, string description, string time, string issue, string issueId)
+            public Entry(string jobName, string jobNumber, string entryDate, string taskName, string ticketNumber, string label, string staffName, string description, double time, int issueID, string issueComment)
             {
                 JobName = jobName;
                 JobNumber = jobNumber;
@@ -247,10 +334,10 @@ namespace TimeSheetParser
                 StaffName = staffName;
                 Description = description;
                 Time = time;
-                Issue = issue;
-                IssueId = issueId;
+                IssueID = issueID;
+                IssueComment = issueComment;
             }
-            public Entry(string jobName, string jobNumber, string entryDate, string taskName, string ticketNumber, string label, string staffName, string description, string time)
+            public Entry(string jobName, string jobNumber, string entryDate, string taskName, string ticketNumber, string label, string staffName, string description, double time)
             {
                 JobName = jobName;
                 JobNumber = jobNumber;
@@ -261,8 +348,8 @@ namespace TimeSheetParser
                 StaffName = staffName;
                 Description = description;
                 Time = time;
-                Issue = "0";
-                IssueId = "-1";
+                IssueID = 0;
+                IssueComment = "";
             }
 
         }
